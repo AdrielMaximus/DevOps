baa8bfc4206a421d7a5472eab3e815d7966e575e
Adriel Maximus <119170526+AdrielMaximus@users.noreply.github.com>
2024-05-01 16:36:04 -0300

    Update read.py

--- Web Server/read.py
+++ Web Server/read.py
@@ -1,6 +1,14 @@
 import pandas as pd
+import json
 file = "Fileexample.csv"
 df = pd.read_csv(file)
 df = pd.read_xml(file)
 df = pd.read_json(file)
 df = pd.read_excel(file)
+#colum
+df.columns = ['Name', 'Phone', 'Number', 'Birthday']
+#Read a Json
+with open('fileexample.json', 'r') as openfile:
+    json_object = json.load(openfile)
+print(json_object) 
+# Read XML
