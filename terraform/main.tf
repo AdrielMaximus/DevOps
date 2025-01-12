70db4b4ae3444fba59ff757b1a0f3afc76d4101a
Adriel Maximus <adrvlogs885@gmail.com>
2025-01-12 14:45:04 -0300

    check

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,15 @@
 terraform {
-    required_
+    required_providers{
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 3.0"
+        }
+    }
+    backend "s3" {
+        
+        key = "terraform.tfstate"
+    }
+}
+provider "aws" {
+    region = "us-east-1"
 }
